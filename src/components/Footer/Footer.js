5493d061270cb7f3f0d979ade35d9bc8968d8668
gh-amanyadav <amankumar99347@gmail.com>
2022-04-29 17:10:34 +0530

    Initial Commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+const Footer = () => (
+  <footer className='footer'>
+    <a
+      href='https://github.com/gh-amanyadav/portfolio'
+      className='link footer__link'
+    >
+      Created By Aman Kumar
+    </a>
+  </footer>
+)
+
+export default Footer
---
+++ src/portfolio.js
@@ -0,0 +1,73 @@
+const header = {
+  // all the properties are optional - can be left empty or deleted
+  homepage: 'https://www.linkedin.com/in/in-amanyadav/',
+  title: 'AKY.',
+}
+
+const about = {
+  // all the properties are optional - can be left empty or deleted
+  name: 'Aman Kumar',
+  role: 'App Developer and Front End Engineer',
+  description:
+    'Multilingual, newly graduated App Developer offering expertise in Android and IOS. Innovative thinker with sound judgment and resourceful approach to problem-solving. Expert in App Development with training in Flutter and Android Native.',
+  resume: 'https://example.com',
+  social: {
+    linkedin: 'https://www.linkedin.com/in/in-amanyadav/',
+    github: 'https://github.com/gh-amanyadav/',
+  },
+}
+
+const projects = [
+  // projects can be added an removed
+  // if there are no projects, Projects section won't show up
+  {
+    name: 'Project 1',
+    description:
+      'Soon..',
+    stack: ['SASS', 'TypeScript', 'React'],
+    sourceCode: 'Soon...',
+    livePreview: 'Soon...',
+  },
+  {
+    name: 'Project 2',
+    description:
+      'Soon...',
+    stack: ['SASS', 'TypeScript', 'React'],
+    sourceCode: 'Soon...',
+    livePreview: 'Soon...',
+  },
+  {
+    name: 'Project 3',
+    description:
+      'Soon...',
+    stack: ['SASS', 'TypeScript', 'React'],
+    sourceCode: 'Soon...',
+    livePreview: 'Soon...',
+  },
+]
+
+const skills = [
+  // skills can be added or removed
+  // if there are no skills, Skills section won't show up
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'Flutter',
+  'React',
+  'Dart',
+  'C',
+  'C++',
+  'Kotlin',
+  'Python',
+  'Git/Github',
+  'Vs Code',
+  'Android Studio',
+  'PostMan'
+]
+
+const contact = {
+  // email is optional - if left empty Contact section won't show up
+  email: 'amankumar99347@gmail.com',
+}
+
+export { header, about, projects, skills, contact }
